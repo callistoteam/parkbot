c1c3d2a49a0e1b348c36f01123358068fe2ff96f
Yoruroong <tuntun4132@naver.com>
2020-07-31 22:58:21 +0900

    file

---
+++ src/commands/music/play.js
@@ -0,0 +1,10 @@
+const Command = require("../../structures/Command");
+
+module.exports = class Ping extends Command {
+    alias = [ "p", 'ㅔㅣ묘' ]
+    permission = 0
+
+    async execute({ client, message }){
+        // gdgd
+    }
+}
