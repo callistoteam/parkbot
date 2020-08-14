7c3c7fe7b6820498ebc5a329a89a859be6086d4a
Yoruroong <tuntun4132@naver.com>
2020-08-14 17:03:55 +0900

    feat: 자동 배포

---
+++ autopull.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line node/no-extraneous-require
+const fetch = require('node-fetch')
+
+fetch(process.env.WEBHOOK, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: '{"req": "pull"}'
+}).then(res => console.log(JSON.stringify(res.json())))
