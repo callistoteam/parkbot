67810576a8657d1db4cc54dd652382be0db191f3
Yoruroong <tuntun4132@naver.com>
2020-08-18 13:10:04 +0900

    feat: resume

---
+++ src/commands/music/resume.js
@@ -0,0 +1,18 @@
+const Command = require('../../structures/Command')
+
+module.exports = class Resume extends Command {
+    constructor(client){
+        super(client)
+        this.alias = [ 'resume' ]
+        this.permission = 0x0
+        this.category = 'music'
+    }
+
+    async execute({ client, message }){
+        const player = await client.music.playerCollection.get(message.guild.id)
+        if(!player) return message.reply('이 서버에서 재생중인 음악이 없어!')
+
+        player.resume()
+        message.channel.send("음악을 다시 재생할께!")
+    }
+}
