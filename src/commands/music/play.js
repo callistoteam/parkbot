6a0f5e614b2b5879fef1af65ad14762af3bea796
Yoruroong <tuntun4132@naver.com>
2020-09-03 09:35:35 +0900

    fix: bug

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -42,7 +42,7 @@ module.exports = class Play extends Command {
                     deafen: true
                 },
                 {
-                    skipOnError: false
+                    skipOnError: true
                 }
             )
         }
