b80b89ea28e1de1919ce4b3735f5703fc4eb7d67
Yoruroong <tuntun4132@naver.com>
2020-09-11 14:25:24 +0900

    github: pull

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -13,7 +13,7 @@ module.exports = class Help extends Command {
     async execute({ client, message }){
         const embed = new MessageEmbed()
             .setColor('RANDOM')
-            .addField('링크', '[초대하기](https://parkbot.ml)\n[지원 서버](https://discord.gg/jE33mfD)\n[이용약관](https://callisto.team/tos)\n[개인정보 처리방침](https://parkbot.ml/privacy)')
+            .addField('링크', '[초대하기](https://parkbot.ml)\n[지원 서버](https://discord.gg/jE33mfD)\n[이용약관](https://callisto.team/tos)\n[개인정보 처리방침](https://parkbot.ml/privacy)\n**[이벤트 참여하기](https://forms.gle/EHrUD1DZWzdvFXb17)**')
 
         const commands = (category) => {
             return client.commands
@@ -26,6 +26,6 @@ module.exports = class Help extends Command {
             .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
             .reduce((string, category) => string + '\n' + category)
 
-        return message.channel.send(embed.setDescription(info + '\n\n[이벤트 참여하기](https://forms.gle/EHrUD1DZWzdvFXb17)'))
+        return message.channel.send(embed.setDescription(info))
     }
 }
