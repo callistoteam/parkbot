6fd3a57663852e82034f526791eb2b07681035b3
Yoruroong <tuntun4132@naver.com>
2020-09-11 16:22:27 +0900

    fix: bug

--- src/commands/general/embedcolor.js
+++ src/commands/general/embedcolor.js
@@ -11,6 +11,8 @@ module.exports = class Embedcolor extends Command {
     }
 
     async execute({ client, message }){
+        if(message.author.data.premium < new Date()) return message.reply(new Embed(message).premium())
+        if(!message.data.args) return message.reply('HEX코드를 입력해줘!')
         let color = message.data.arg[0].replace('#', '').replace('0x', '')
         if(!isHex(color)) return message.reply('올바른 헥스코드를 입력해줘. \n\n<https://www.google.com/search?q=color+picker> 여기서 자신이 원하는 색을 찾은 후 hex를 복사하면 돼.')
         await client.knex('users').update({color: color}).where('id', message.author.id)
