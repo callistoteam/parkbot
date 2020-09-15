0475362cce010a2609a549b6fab40c47519b0215
Yoruroong <tuntun4132@naver.com>
2020-09-16 00:02:22 +0900

    text: sample eq

--- src/commands/music/seteq.js
+++ src/commands/music/seteq.js
@@ -11,12 +11,22 @@ module.exports = class SetEQ extends Command {
     async execute({ message, player }){
         if(!player) return message.reply('이 서버에서 재생중인 음악이 없어!')
 
-        let entries1 = message.data.arg.join(' ').split('--');
+        if(!message.data.args) return message.reply(`
+이퀄라이저를 설정할 값을 입력해줘. 이퀄라이저를 설정하기 어렵다면 아래의 샘플 값을 들어봐.
+\`#이퀄라이저 --1 -0.25 --1 0.25\` - 저음 강조
+\`#이퀄라이저 --14 1 --14 1\` - 고음강조
+\`#이퀄라이저 --7 1 --7 1\` - 저음을 지나치게 강조
+\`#이퀄라이저 --0 0 --0 0\` - 기본설정
+
+위의 샘플 값들은 개발자가 들어서 이용자의 귀하고는 차이가 있을 수 있어.
+        `)
+
+        let entries1 = message.data.arg.join(' ').split('--')
         entries1.shift();
         let entries = entries1.filter(function(item) {
-            return item !== null && item !== undefined && item !== '';
+            return item !== null && item !== undefined && item !== ''
         })
-        let equaliser = [];
+        let equaliser = []
 
         for (let eq of entries) {
             let band
@@ -24,14 +34,14 @@ module.exports = class SetEQ extends Command {
             [ band, gain ] = eq.trim().split(/ +/g);
 
             if (isNaN(band) || isNaN(gain)) continue;
-            if (parseInt(band) > 14 || parseInt(band) < 0) return message.reply('밴드는 0 에서 14 사이어야해!');
+            if (parseInt(band) > 14 || parseInt(band) < 0) return message.reply('밴드는 0 에서 14 사이어야해!')
             if (parseFloat(gain) < -0.25 || parseFloat(gain) > 1) return message.reply('Gain은 -0.25 에서 1 사이어야해!')
 
-            equaliser.push({ band: parseInt(band), gain: parseFloat(gain) });
+            equaliser.push({ band: parseInt(band), gain: parseFloat(gain) })
         }
 
-        equaliser.length ? player.EQBands(equaliser) : player.EQBands();
+        equaliser.length ? player.EQBands(equaliser) : player.EQBands()
 
-        message.reply('이퀄라이저가 업데이트되었어.')
+        message.reply('이퀄라이저가 업데이트되었어. (적용까지 5~10초 소요)')
     }
 }
