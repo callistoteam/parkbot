6360a1150817f0dfbb281d7b3d20c6276624d241
Yoruroong <tuntun4132@naver.com>
2020-09-20 16:44:57 -0700

    fix(help.js): text(dns dev)

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -22,7 +22,7 @@ module.exports = class Help extends Command {
                 .join(', ')
         }
 
-        const info = ['dev', 'general', 'music', 'useful']
+        const info = ['general', 'music', 'useful']
             .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
             .reduce((string, category) => string + '\n' + category)
 
