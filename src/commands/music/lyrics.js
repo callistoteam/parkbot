9afbe976432a9e152967d3ee34ccf9669f75b05e
Yoruroong <tuntun4132@naver.com>
2020-09-20 01:16:35 -0700

    feat: lyrics

---
+++ src/commands/music/lyrics.js
@@ -0,0 +1,27 @@
+const { Command, Embed } = require('../../utils')
+const Lyrics = require('slyrics')
+const lyrics = new Lyrics()
+
+async function search (provider, title) {
+    const result = await lyrics.get(provider, title)
+    return result
+}
+
+module.exports = class Lyrics extends Command {
+    constructor(client){
+        super(client)
+        this.alias = [ '가사', 'lyrics' ]
+        this.permission = 0x0
+        this.category = 'music'
+    }
+
+    async execute({ message }){
+        if(!message.data.args) return message.reply('가사를 검색할 음악 제목을 입력해줘!')
+        try{
+            message.channel.send(new Embed(message).lyrics(await search('melon', message.data.args)))
+        // eslint-disable-next-line node/no-unsupported-features/es-syntax
+        } catch {
+            message.channel.send('검색된 음악이 없어. 확인 후 다시 시도해줘.')
+        }
+    }
+}
