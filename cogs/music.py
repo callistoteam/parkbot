664c9f8323d2cb4e80c7a73b86167d1bc060e2e1
Yoruroong <tuntun4132@naver.com>
2020-10-14 16:37:26 +0200

    stop, skip, volume..?

--- cogs/music.py
+++ cogs/music.py
@@ -44,15 +44,14 @@ class Music(commands.Cog):
 
             await self.Audio.connect(ctx.author.voice.channel)
             return await ctx.send(
-                f"플레이어가 생성되어 있지 않아서 플레이어를 생성하고 {ctx.author.voice.channel.mention} 채널에 연결했어.\n`{ctx.content}`커맨드를 다시 실행해줘."
+                f"플레이어가 생성되어 있지 않아서 플레이어를 생성하고 {ctx.author.voice.channel.mention} 채널에 연결했어.\n`{' '.join(args)}`커맨드를 다시 실행해줘."
             )
 
         if not hasattr(vc, "channel"):
             vc.channel = ctx.channel
 
         #ctx.content[6:].strip()
-        Source = await vc.loadSource('yorushika')
-        
+        Source = await vc.loadSource(' '.join(args))
 
         if isinstance(Source, list):
             return await ctx.send(
@@ -60,6 +59,38 @@ class Music(commands.Cog):
             )
         else:
             return await ctx.send(f'{Source["title"]}을(를) 대기열에 추가했어.')
+
+    @commands.command()
+    async def stop(self, ctx):
+        try:
+            vc = self.Audio.getVC(ctx.guild)
+            await vc.destroy()
+            return await ctx.send(f"플레이어를 삭제하고 대기열을 초기화했어.")
+        except:
+            return await ctx.send("`#join`커맨드를 통해 플레이어를 먼저 생성해줘.")
+
+    @commands.command()
+    async def skip(self, ctx):
+        try:
+            vc = self.Audio.getVC(ctx.guild)
+            Remain = await vc.skip(1)
+            return await ctx.send(f"재생중인 음악을 스킵했어. {Remain}개의 음악이 대기열에 남아있어.")
+        except:
+            return await ctx.send("`#join`커맨드를 통해 플레이어를 먼저 생성해줘.")
+
+    @commands.command()
+    async def volume(self, ctx, volume):
+        try:
+            vc = self.Audio.getVC(ctx.guild)
+            offset = (
+                int(volume.strip()) if volume.strip() else 100
+            )
+
+            Volume = await vc.setVolume(offset / 100)
+            return await ctx.send(f"{Volume * 100}%로 볼륨을 설정했어.")
+        except:
+            return await ctx.send("`#join`커맨드를 통해 플레이어를 먼저 생성해줘.")
+            
 
 def setup(client):
     client.add_cog(Music(client))
