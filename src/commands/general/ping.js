9ca20c0cff711042a5ce0a8f81f03a1aa38b9a2d
Yoruroong <tuntun4132@naver.com>
2020-11-09 09:54:56 +0900

    feat: ping

---
+++ src/commands/general/ping.js
@@ -0,0 +1,28 @@
+const { Command } = require('../../utils')
+const Discord = require('discord.js')
+
+module.exports = class Ping extends Command {
+    constructor(client){
+        super(client)
+        this.alias = [ '핑', 'ping' ]
+        this.permission = 0x0
+        this.category = 'general'
+    }
+
+    async execute({ client, message }){
+        try{
+            await message.channel.send('잠시만 기다려주세요').then(async msg => {
+                let pingembed = new Discord.MessageEmbed()
+                    .setTitle('퐁!')
+                    .setColor('RANDOM')
+                    .addField('지연 시간', `${msg.createdTimestamp - message.createdTimestamp}ms`)
+                    .addField('API지연시간', `${client.ws.ping}ms`)
+                pingembed.setTimestamp()
+                msg.edit('', pingembed)
+            })
+        }catch(e) {
+            message.reply('알 수 없는 오류가 발생했어요.')
+            console.log(e)
+        }
+    }
+}
