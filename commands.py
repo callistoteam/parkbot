26c359b2e31efbfce04b9c36e23b48d5ef599383
Yoruroong <tuntun4132@naver.com>
2020-12-16 09:16:24 +0900

    feat: autoplay

---
+++ commands.py
@@ -0,0 +1,4 @@
+play = ["재생", 'play', 'p']
+stop = ["정지", "stop", "st"]
+volume = ["볼륨", "음량", "volume", "v"]
+autoplay = ["추천영상", "autoplay", "auto"]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import config
+import commands
 import db
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -43,7 +44,7 @@ async def on_message(message):
         ping = now*1000
         await msg.edit(content=f"{ping}ms")
 
-    if command == "play":
+    if command in commands.play:
         if not message.author.voice:
             return await message.channel.send("> 먼저 보이스 채널에 들어가줘.")
 
@@ -56,6 +57,9 @@ async def on_message(message):
         if not hasattr(vc, "channel"):
             vc.channel = message.channel
 
+        if not args[1]:
+            return await message.channel.send(f"> 올바른 사용법: `{args[0]} [음악 제목(또는 URL) - 필수]`")
+
         Source = await vc.loadSource(" ".join(args[1:]))
 
         if isinstance(Source, list):
@@ -66,7 +70,7 @@ async def on_message(message):
             print(Source)
             return await message.channel.send(f'{Source["data"]["title"]} 이(가) 추가되었어.')
 
-    if command == "stop":
+    if command in commands.stop:
         vc = Audio.getVC(message.guild)
 
         if not vc:
@@ -76,7 +80,7 @@ async def on_message(message):
 
         return await message.channel.send(f"큐를 초기화하고 보이스 채널을 나갔어.")
 
-    if command == "volume":
+    if command in commands.volume:
         vc = Audio.getVC(message.guild)
 
         if not vc:
@@ -89,6 +93,23 @@ async def on_message(message):
         Volume = await vc.setVolume(offset / 100)
 
         return await message.channel.send(f"> 볼륨을 `{Volume * 100}%`로 설정했어.")
+
+    if command in commands.autoplay:
+        vc = Audio.getVC(message.guild)
+
+        if not vc:
+            return await message.channel.send(ns)
+
+        offset = (
+            int(args[1]) if args[1] else "on"
+        )
+        offset = {"on": True, "off": False}.get(offset, True)
+
+        autoplay = await vc.setAutoplay(offset)
+
+        return await message.channel.send(
+            f'추천 영상 재생이 {"활" if autoplay else "비활"}성화 되었어.'
+        )
 
     if command == "logout":
         if not message.author.id in config.owner: return
